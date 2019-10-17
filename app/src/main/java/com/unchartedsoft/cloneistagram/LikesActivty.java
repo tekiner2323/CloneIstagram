e986cef1347d71d64a4971905697f61526d7d24a
tekiner2323 <tekiner2323@users.noreply.github.com>
2019-10-17 15:47:37 +0300

    Bottom Navigation Bar ve Activty ler eklendi.
    Navigation Bar üzerinde değişme işlemleri yapıldı.

---
+++ app/src/main/java/com/unchartedsoft/cloneistagram/LikesActivty.java
@@ -0,0 +1,43 @@
+package com.unchartedsoft.cloneistagram;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.unchartedsoft.cloneistagram.Utiles.BottomNavigationViewHelper;
+
+public class LikesActivty extends AppCompatActivity {
+    private static final String TAG = "LikesActivty";
+    private static final int Activity_Num =3;
+
+    private Context mContext =  LikesActivty.this;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG,"onCreate: started");
+
+        setupBottomNavigationView();
+    }
+
+    private void setupBottomNavigationView() {
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavigationBar);
+        BottomNavigationViewHelper.settupBottomNavigationView(bottomNavigationViewEx);
+
+        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
+
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(Activity_Num);
+        menuItem.setChecked(true);
+
+    }
+}
+
---
+++ app/src/main/java/com/unchartedsoft/cloneistagram/SearchActivty.java
@@ -0,0 +1,44 @@
+package com.unchartedsoft.cloneistagram;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.unchartedsoft.cloneistagram.Utiles.BottomNavigationViewHelper;
+
+public class SearchActivty extends AppCompatActivity {
+    private static final String TAG = "SearchActivty";
+    private static final int Activity_Num =1;
+
+    private Context mContext =  SearchActivty.this;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG,"onCreate: started");
+
+        setupBottomNavigationView();
+    }
+
+    private void setupBottomNavigationView() {
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavigationBar);
+        BottomNavigationViewHelper.settupBottomNavigationView(bottomNavigationViewEx);
+
+        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
+
+
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(Activity_Num);
+        menuItem.setChecked(true);
+
+    }
+}
+
